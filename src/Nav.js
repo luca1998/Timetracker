41292c4c06bd6d5e77f9ec20c382f46f0cd85ecc
luca1998 <tschanz.luca@gmail.com>
2019-08-13 13:12:51 +0200

    the rest

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './nav.css';
+import SideNav from 'react-simple-sidenav';
 import {
     Route,
     NavLink,
@@ -12,20 +14,24 @@ import Ferienuebersicht from "./Ferienuebersicht"
 class Nav extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <div>
-                    <ol id="test">
-                        <li><NavLink to="/ZeitEintragen">ZeitEintragen</NavLink></li>
-                        <li><NavLink to="/ZeitUebersicht">ZeitUebersicht</NavLink></li>
-                        <li><NavLink to="/Ferienuebersicht">Ferienuebersicht</NavLink></li>
-                    </ol>
-                    <div className="content">
-                        <Route path="/ZeitEintragen" component={ZeitEintragen} />
-                        <Route path="/ZeitUebersicht" component={ZeitUebersicht} />
-                        <Route path="/Ferienuebersicht" component={Ferienuebersicht} />
+            <SideNav>
+                showNav =  {this.state.showNav}
+                onHideNav =  {() => this.setState({ showNav: false })}
+                <BrowserRouter>
+                    <div>
+                        <ol id="test">
+                            items =  {[<li><NavLink to="/ZeitEintragen">ZeitEintragen</NavLink></li>,
+                            <li><NavLink to="/ZeitUebersicht">ZeitUebersicht</NavLink></li>,
+                            <li><NavLink to="/Ferienuebersicht">Ferienuebersicht</NavLink></li>]}
+                        </ol>
+                        <div className="content">
+                            <Route path="/ZeitEintragen" component={ZeitEintragen} />
+                            <Route path="/ZeitUebersicht" component={ZeitUebersicht} />
+                            <Route path="/Ferienuebersicht" component={Ferienuebersicht} />
+                        </div>
                     </div>
-                </div>
-            </BrowserRouter>
+                </BrowserRouter>
+            </SideNav>
         );
     }
 }
