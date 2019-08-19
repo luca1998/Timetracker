a9dd5791be092f0b27f4eb5faaecccd438cf208f
luca1998 <tschanz.luca@gmail.com>
2019-08-19 12:54:05 +0200

    latest nav changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './App.css';
 import Nav from './Nav';
-import SideNav, {MenuIcon} from 'react-simple-sidenav';
-import { tsConstructorType } from '@babel/types';
 
 function App() {
 
- 
+
   return (
-    
+
     <div className="App">
-     
-            <Nav /> 
-       
+
+      <Nav />
+
 
     </div>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './nav.css';
-import SideNav, {MenuIcon} from 'react-simple-sidenav';
+import SideNav, { MenuIcon } from 'react-simple-sidenav';
 import {
     Route,
     NavLink,
@@ -12,29 +12,41 @@ import Ferienuebersicht from "./Ferienuebersicht"
 
 
 class Nav extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            showNav: true,
+        }
+    }
+
     render() {
         return (
-                <BrowserRouter>
-                    <MenuIcon onClick={() => this.setState({showNav: true})}/>
-                     <SideNav
-                         showNav = {true}
-                         onHideNav = {() => this.setState({showNav: false})}
-                 
-                             />
-                    <div>
-                        <ol id="test">
-                            <li><NavLink to="/ZeitEintragen">ZeitEintragen</NavLink></li>
-                            <li><NavLink to="/ZeitUebersicht">ZeitUebersicht</NavLink></li>
-                            <li><NavLink to="/Ferienuebersicht">Ferienuebersicht</NavLink></li>
-                        </ol>
-                        <div className="content">
-                            <Route path="/ZeitEintragen" component={ZeitEintragen} />
-                            <Route path="/ZeitUebersicht" component={ZeitUebersicht} />
-                            <Route path="/Ferienuebersicht" component={Ferienuebersicht} />
-                        </div>
+
+            <BrowserRouter>
+
+                <MenuIcon onClick={() => this.setState({ showNav: true })}
+                    style={{ verticalAlign: 'middle', padding: '12px' }} />
+                <SideNav
+                    showNav={this.state.showNav}
+                    onHideNav={() => this.setState({ showNav: false })}
+
+
+                />
+                <div>
+                    <ol id="test">
+                        <li><NavLink to="/ZeitEintragen">ZeitEintragen</NavLink></li>
+                        <li><NavLink to="/ZeitUebersicht">ZeitUebersicht</NavLink></li>
+                        <li><NavLink to="/Ferienuebersicht">Ferienuebersicht</NavLink></li>
+                    </ol>
+                    <div className="content">
+                        <Route path="/ZeitEintragen" component={ZeitEintragen} />
+                        <Route path="/ZeitUebersicht" component={ZeitUebersicht} />
+                        <Route path="/Ferienuebersicht" component={Ferienuebersicht} />
                     </div>
-                </BrowserRouter>
-            
+                </div>
+            </BrowserRouter>
+
         );
     }
 }
